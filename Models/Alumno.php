f1d384fa2097d4e5497c05ce0640ef5f690df9b2
Herson95 <hostinmarvish@gmail.com>
2018-09-17 21:06:17 -0600

    Model Alumno

--- Models/Alumno.php
+++ Models/Alumno.php
@@ -4,14 +4,7 @@ require_once "Config/Database.php";
 class Alumno
 {
     private $pdo;
-	public $id;
-    public $Nombre;
-    public $Apellido;
-    public $Sexo;
-    public $FechaRegistro;
-    public $FechaNacimiento;
-    public $Foto;
-    public $Correo;
+
 
 	public function __CONSTRUCT()
 	{
@@ -25,100 +18,5 @@ class Alumno
 		}
 	}
 
-	public function Listar()
-	{
-		try
-		{
-			$result = array();
-
-			$stm = $this->pdo->prepare("SELECT * FROM tbl_alumnos");
-			$stm->execute();
-
-			return $stm->fetchAll(PDO::FETCH_OBJ);
-		}
-		catch(Exception $e)
-		{
-			die($e->getMessage());
-		}
-	}
-
-	public function Obtener($id)
-	{
-		try 
-		{
-			$stm = $this->pdo
-			          ->prepare("SELECT * FROM tbl_alumnos WHERE AlumnoID = ?");
-			          
-
-			$stm->execute(array($id));
-			return $stm->fetch(PDO::FETCH_OBJ);
-		} catch (Exception $e) 
-		{
-			die($e->getMessage());
-		}
-	}
-
-	public function Eliminar($id)
-	{
-		try 
-		{
-			$stm = $this->pdo
-			            ->prepare("DELETE FROM tbl_alumnos WHERE id = ?");			          
-
-			$stm->execute(array($id));
-		} catch (Exception $e) 
-		{
-			die($e->getMessage());
-		}
-	}
-
-	public function Actualizar($data)
-	{
-		try 
-		{
-			$sql = "UPDATE tbl_alumnos SET 
-						Nombre        = ?, 
-						Apellido      = ?,
-						Sexo          = ?, 
-						FechaNacimiento = ?
-				    WHERE id = ?";
-
-			$this->pdo->prepare($sql)
-			     ->execute(
-				    array(
-                        $data->Nombre, 
-                        $data->Apellido,
-                        $data->Sexo,
-                        $data->FechaNacimiento,
-                        $data->id
-					)
-				);
-		} catch (Exception $e) 
-		{
-			die($e->getMessage());
-		}
-	}
-
-	public function Registrar(Alumno $data)
-	{
-		try 
-		{
-		$sql = "INSERT INTO tbl_alumnos (Nombre,Apellido,Genero,FechaNacimiento) 
-		        VALUES (?, ?, ?, ?)";
-
-		$this->pdo->prepare($sql)
-		     ->execute(
-				array(
-                    $data->Nombre,                  
-                    $data->Apellido, 
-                    $data->Sexo,
-                    $data->FechaNacimiento
-                )
-			);
-		} catch (Exception $e) 
-		{
-			die($e->getMessage());
-		}
-	}
 }
 ?>
