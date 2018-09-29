a2b02b87b0c2ab731ee3664ce0b8b87a1235df80
bladimir100 <43359248+bladimir100@users.noreply.github.com>
2018-09-29 15:44:58 -0600

    Update Profesor.php

--- Models/Profesor.php
+++ Models/Profesor.php
@@ -1,117 +1,110 @@
 <?php
 //Este modelo lo realizara Adonis
 require_once "Config/Database.php";
-class Profesor
-{
-    private $pdo;
-
-    public $ProfesorID;
+class Profesor{
+	private $pdo;
+	public $ProfesorID;
     public $Nombre;
     public $Apellido;
     public $Dui;
     public $Telefono;
     public $Estado;
-
     public function __CONSTRUCT()
-    {
-        try
-        {
-            $this->pdo = Database::StartUp();
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
+	{
+		try
+		{
+			$this->pdo = Database::StartUp();     
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
 
-    public function Listar()
-    {
-        try
-        {
-            $result = array();
+	public function Listar(){
+		try{
+			$result = array();
 
-            $stm = $this->pdo->prepare("SELECT * FROM tbl_profesores");
-            $stm->execute();
+			$sql = 'CALL READ_PROFESOR()';
+			$stm = $this->pdo->prepare($sql);
+			$stm->execute();
+			return $stm->fetchAll(PDO::FETCH_OBJ);
+		}
+		catch(Exception $e){
+			die($e->getMessage());
+		}
+	}
 
-            return $stm->fetchAll(PDO::FETCH_OBJ);
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
+	public function Obtener($ProfesorID){
+		try{
+			$sql =' CALL READ_PROFESOR_ID(:ProfesorID)';
+			$stm = $this->pdo->prepare($sql);
+			$stm->bindParam(':ProfesorID', $ProfesorID, PDO::PARAM_INT);
+			$stm->execute();
+			return $stm->fetch(PDO::FETCH_OBJ);
+		}catch(Exception $e){
+			die($e->getMessage());
+		}
+	}
 
-    public function Obtener($id)
-    {
-        try
-        {
-            $stm = $this->pdo
-                ->prepare("SELECT * FROM tbl_profesores WHERE ProfesorID = ?");
+	public function Eliminar($ProfesorID)
+	{
+		try 
+		{
+			$sql = "CALL DELETE_PROFESOR($ProfesorID)";
+			$stm = $this->pdo->prepare($sql);
+			$stm->execute();
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
 
-            $stm->execute(array($id));
-            return $stm->fetch(PDO::FETCH_OBJ);
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
+	public function Actualizar($data)
+	{
+		try 
+		{
 
-    public function Eliminar($id)
-    {
-        try
-        {
-            $stm = $this->pdo
-                ->prepare("DELETE FROM tbl_profesores WHERE ProfesorID = ?");
+			$sql = 'CALL UPDATE_PROFESOR(?,?,?)';
 
-            $stm->execute(array($id));
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
-
-    public function Actualizar($data)
-    {
-        try
-        {
-            $sql = "UPDATE tbl_profesores SET
-						Nombre        = ?,
-						Apellido      = ?,
-                        Dui           = ?,
-						Telefono      = ?,
-						Estado        = ?
-				    WHERE ProfesorID = ?";
-
-            $this->pdo->prepare($sql)
-                ->execute(
-                    array(
-                        $data->Nombre,
+			$this->pdo->prepare($sql)
+			     ->execute(
+				    array(
+						$data->Nombre, 
                         $data->Apellido,
                         $data->Dui,
                         $data->Telefono,
                         $data->Estado,
-                        $data->ProfesorID,
-                    )
-                );
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
+                        $data->ProfesorID
+					)
+				);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
 
-    public function Registrar(Profesor $data)
-    {
-        try
-        {
-            $sql = "INSERT INTO tbl_profesores (Nombre,Apellido,Dui,Telefono,Estado)
-		        VALUES (?, ?, ?, ?, ?)";
-
-            $this->pdo->prepare($sql)
-                ->execute(
-                    array(
-                        $data->Nombre,
-                        $data->Apellido,
-                        $data->Dui,
-                        $data->Telefono,
-                        $data->Estado,
-
-                    )
-                );
-        } catch (Exception $e) {
-            die($e->getMessage());
-        }
-    }
+	public function Registrar(Profesor $data)
+	{
+		try 
+		{
+			$sql = 'CALL INSERT_PROFESOR(?,?)';
+			$this->pdo->prepare($sql)
+		     ->execute(
+				array(
+					$data->Nombre, 
+					$data->Apellido,
+					$data->Dui,
+					$data->Telefono,
+					$data->Estado,
+					$data->ProfesorID
+                )
+			);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
 }
+
+?>
